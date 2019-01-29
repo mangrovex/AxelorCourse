c65df7e89f79d802b29408ff508b06689043cbec
mangrovex <35317956+mangrovex@users.noreply.github.com>
2019-01-29 11:14:51 -0500

    birt commit

---
+++ SESION-3/Birt-Tutorial/sql/cross-tab.sql
@@ -0,0 +1,56 @@
+CREATE TABLE student_marks
+(
+    student_name VARCHAR(100) NOT NULL,
+    course VARCHAR(100) NOT NULL,
+    marks INT(11) NOT NULL
+);
+
+INSERT INTO birt_tutorial.student_marks (student_name, course, marks) VALUES ('Barbara', 'Chemistry', 73);
+INSERT INTO birt_tutorial.student_marks (student_name, course, marks) VALUES ('Elizabeth', 'Chemistry', 94);
+INSERT INTO birt_tutorial.student_marks (student_name, course, marks) VALUES ('James', 'Chemistry', 94);
+INSERT INTO birt_tutorial.student_marks (student_name, course, marks) VALUES ('John', 'Chemistry', 68);
+INSERT INTO birt_tutorial.student_marks (student_name, course, marks) VALUES ('Linda', 'Chemistry', 53);
+INSERT INTO birt_tutorial.student_marks (student_name, course, marks) VALUES ('Mary', 'Chemistry', 97);
+INSERT INTO birt_tutorial.student_marks (student_name, course, marks) VALUES ('Michael', 'Chemistry', 73);
+INSERT INTO birt_tutorial.student_marks (student_name, course, marks) VALUES ('Patricia', 'Chemistry', 81);
+INSERT INTO birt_tutorial.student_marks (student_name, course, marks) VALUES ('Robert', 'Chemistry', 72);
+INSERT INTO birt_tutorial.student_marks (student_name, course, marks) VALUES ('William', 'Chemistry', 66);
+INSERT INTO birt_tutorial.student_marks (student_name, course, marks) VALUES ('Elizabeth', 'Drama', 45);
+INSERT INTO birt_tutorial.student_marks (student_name, course, marks) VALUES ('James', 'Drama', 39);
+INSERT INTO birt_tutorial.student_marks (student_name, course, marks) VALUES ('John', 'Drama', 75);
+INSERT INTO birt_tutorial.student_marks (student_name, course, marks) VALUES ('Robert', 'Drama', 69);
+INSERT INTO birt_tutorial.student_marks (student_name, course, marks) VALUES ('Linda', 'Drawing', 77);
+INSERT INTO birt_tutorial.student_marks (student_name, course, marks) VALUES ('Mary', 'Drawing', 41);
+INSERT INTO birt_tutorial.student_marks (student_name, course, marks) VALUES ('Michael', 'Drawing', 77);
+INSERT INTO birt_tutorial.student_marks (student_name, course, marks) VALUES ('Patricia', 'Drawing', 87);
+INSERT INTO birt_tutorial.student_marks (student_name, course, marks) VALUES ('William', 'Drawing', 40);
+INSERT INTO birt_tutorial.student_marks (student_name, course, marks) VALUES ('Barbara', 'History', 94);
+INSERT INTO birt_tutorial.student_marks (student_name, course, marks) VALUES ('Elizabeth', 'History', 78);
+INSERT INTO birt_tutorial.student_marks (student_name, course, marks) VALUES ('James', 'History', 90);
+INSERT INTO birt_tutorial.student_marks (student_name, course, marks) VALUES ('John', 'History', 62);
+INSERT INTO birt_tutorial.student_marks (student_name, course, marks) VALUES ('Linda', 'History', 59);
+INSERT INTO birt_tutorial.student_marks (student_name, course, marks) VALUES ('Michael', 'History', 68);
+INSERT INTO birt_tutorial.student_marks (student_name, course, marks) VALUES ('Patricia', 'History', 65);
+INSERT INTO birt_tutorial.student_marks (student_name, course, marks) VALUES ('Robert', 'History', 87);
+INSERT INTO birt_tutorial.student_marks (student_name, course, marks) VALUES ('Barbara', 'Maths', 59);
+INSERT INTO birt_tutorial.student_marks (student_name, course, marks) VALUES ('Elizabeth', 'Maths', 68);
+INSERT INTO birt_tutorial.student_marks (student_name, course, marks) VALUES ('James', 'Maths', 57);
+INSERT INTO birt_tutorial.student_marks (student_name, course, marks) VALUES ('John', 'Maths', 60);
+INSERT INTO birt_tutorial.student_marks (student_name, course, marks) VALUES ('Linda', 'Maths', 92);
+INSERT INTO birt_tutorial.student_marks (student_name, course, marks) VALUES ('Mary', 'Maths', 45);
+INSERT INTO birt_tutorial.student_marks (student_name, course, marks) VALUES ('Michael', 'Maths', 82);
+INSERT INTO birt_tutorial.student_marks (student_name, course, marks) VALUES ('Patricia', 'Maths', 59);
+INSERT INTO birt_tutorial.student_marks (student_name, course, marks) VALUES ('Robert', 'Maths', 20);
+INSERT INTO birt_tutorial.student_marks (student_name, course, marks) VALUES ('William', 'Maths', 53);
+INSERT INTO birt_tutorial.student_marks (student_name, course, marks) VALUES ('Barbara', 'Music', 70);
+INSERT INTO birt_tutorial.student_marks (student_name, course, marks) VALUES ('Elizabeth', 'Music', 63);
+INSERT INTO birt_tutorial.student_marks (student_name, course, marks) VALUES ('Linda', 'Music', 54);
+INSERT INTO birt_tutorial.student_marks (student_name, course, marks) VALUES ('Mary', 'Music', 98);
+INSERT INTO birt_tutorial.student_marks (student_name, course, marks) VALUES ('Patricia', 'Music', 75);
+INSERT INTO birt_tutorial.student_marks (student_name, course, marks) VALUES ('William', 'Music', 77);
+INSERT INTO birt_tutorial.student_marks (student_name, course, marks) VALUES ('James', 'Physics', 66);
+INSERT INTO birt_tutorial.student_marks (student_name, course, marks) VALUES ('John', 'Physics', 57);
+INSERT INTO birt_tutorial.student_marks (student_name, course, marks) VALUES ('Mary', 'Physics', 44);
+INSERT INTO birt_tutorial.student_marks (student_name, course, marks) VALUES ('Michael', 'Physics', 45);
+INSERT INTO birt_tutorial.student_marks (student_name, course, marks) VALUES ('Robert', 'Physics', 78);
+INSERT INTO birt_tutorial.student_marks (student_name, course, marks) VALUES ('William', 'Physics', 45);
